17782638c4575c6a13f531b4eb9c7808833e4694
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-16 10:18:59 +0900

    swich break안함

---
+++ 202/C기본/03 switch.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int x = 1;
+	//break가 없어서 "12d"가 출력된다.
+	switch (x)
+	{
+	case 0:
+		printf("0");
+	case 1:
+		printf("1");
+	case 2:
+		printf("2");
+	default:
+		printf("d");
+	}
+}
