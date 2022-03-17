2913aef9f227466648e6673908647c63d5ec1d39
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-17 12:10:04 +0900

    endl객체로 개행

--- 202/C++기본/기본.cpp
+++ 202/C++기본/기본.cpp
@@ -1,10 +1,11 @@
-//cout 객체를 가지고 있음
+//cout, endl 객체를 가지고 있음
 #include <iostream>
 
 int main(void) 
 {
 	//printf를 cout 객체로
-	std::cout << "안녕 세상아 \n";
+	//endl객체로 개행
+	std::cout << "안녕 세상아"<< std::endl;
 
 	return 0;
 }
