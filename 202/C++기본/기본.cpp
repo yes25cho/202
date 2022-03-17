703f77669ec4807f934a69cecf8d7c15b8739fc9
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-17 12:06:17 +0900

    printf를 cout 객체로

---
+++ 202/C++기본/기본.cpp
@@ -0,0 +1,10 @@
+//cout 객체를 가지고 있음
+#include <iostream>
+
+int main(void) 
+{
+	//printf를 cout 객체로
+	std::cout << "안녕 세상아\n";
+
+	return 0;
+}
---
+++ 202/C기본/05. 논리연산자.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	if (++a || ++b && ++c) {
+		printf("참.");
+	}
+	printf("%d %d %d", a, b, c);
+
+	return 0;
+}
+
