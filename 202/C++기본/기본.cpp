af4de0b2be8d3904a8f0798156edc40dc8abc0d5
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-17 12:12:35 +0900

    std::  안써도 된다,.

--- 202/C++기본/기본.cpp
+++ 202/C++기본/기본.cpp
@@ -1,11 +1,13 @@
 //cout, endl 객체를 가지고 있음
 #include <iostream>
+//std::  안써도 된다,.
+using namespace std;
 
 int main(void) 
 {
 	//printf를 cout 객체로
 	//endl객체로 개행
-	std::cout << "안녕 세상아"<< std::endl;
+	cout << "안녕 세상아"<< endl;
 
 	return 0;
 }
