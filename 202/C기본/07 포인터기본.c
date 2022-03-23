dd6d4ae7de456c1cb9e2517c62e581c2e31543e1
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-23 09:02:00 +0900

    07 포인터 기본

---
+++ 202/C기본/07 포인터기본.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+
+int main(void)
+{
+	int num1 = 10, num2 = 20, num3 = 30;
+	
+	//ptr1▶num1, ptr2▶num2, ptr3▶num3
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* ptr3 = &num3;
+
+	ptr3 = ptr2;	//prt3▶num2
+	ptr2 = ptr1;	//ptr2▶num1
+	ptr1 = ptr2;	//ptr1▶num1
+
+	printf("%d %d %d \n", *ptr1, *ptr2, *ptr3);
+
+	return 0;
+
+}
+
