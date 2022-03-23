49376fe8bae02c4954a0ff63d3fd69dc0cf469af
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-23 09:20:39 +0900

    포인터 연산

---
+++ 202/C기본/08 포인터기본.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+
+int main(void)
+{
+	int num1 = 15, num2 = 30, num3 = 45;
+	
+	//ptr1▶num1, ptr2▶num2, ptr3▶num3
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* ptr3 = &num3;
+
+	ptr1 = ptr2;	//ptr1▶num2
+	ptr2 = ptr3;	//ptr2▶num3
+
+	*ptr3 *= 2;			//num3 *= 2		-> num3 : 90
+	*ptr1 += *ptr3;		//num2 += num3	-> num2 : 120
+	*ptr2 *= 2;			//num3 *= 2		-> num3 : 180
+
+	printf("%d %d %d \n", *ptr1, *ptr2, *ptr3);
+
+	return 0;
+
+}
+
---
+++ 202/C기본/09 포인터기본.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+
+int main(void)
+{
+	/*만약에 nums[0]의 주소가 0x100이라면?*/
+	int nums[5] = { 10, 20, 30, 40, 50 };
+	int* ptr1 = nums + 2;	//ptr1 : 100 + 4*2= 108
+	int* ptr2 = &nums[0] + 2; //ptr2: 100 + 4 * 2 = 108
+
+	return 0;
+
+}
+
