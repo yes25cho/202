fe240d046ac4737092c1fe1213400209ccba9aa5
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-23 09:55:36 +0900

    Call By Value로 jum의 x,y값이 바뀌진 않음

---
+++ 202/C기본/10 구조체 CallBy포인터.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+typedef struct {
+	float x;
+	float y;
+}Jum;
+
+void AddValueToPoint(Jum p);
+
+int main(void)
+{
+	Jum jum;
+	jum.x = 5.0f;
+	jum.y = 10.0f;
+	//Call By Value로 jum의 x,y값이 바뀌진 않음
+	AddValueToPoint(jum);
+	printf(" % .1f, % .1f\n", jum.x, jum.y);
+
+	return 0;
+
+}
+
+void AddValueToPoint(Jum j) {
+	j.x += 10.0f;
+	j.y += 10.0f;
+}
