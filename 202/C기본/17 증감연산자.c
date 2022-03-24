ab4d7d0f1414ff30867fafb97f4da768a1a07780
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-24 10:52:17 +0900

    17증감 연산자

---
+++ 202/C기본/17 증감연산자.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c = 0;
+
+	//c가 먼저 1증가, c : 1, a : 1
+	a = ++c;	
+	//b에 c값을 먼저 대입하고 c가 1증가, b:1, c:2 
+	b = c++;	
+
+	printf("%d %d %d \n", a, b, ++c);
+
+	return 0;
+}
