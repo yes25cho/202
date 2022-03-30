67c83a52fcc5f3ead0be24a0a388bf2f4224cf23
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-30 10:14:01 +0900

    일반적이 지역변수

---
+++ 202/C기본/static 지역변수.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+void jiyuk(void) {
+	//지역변수는 함수 호출이 끝나면 사라짐
+	int a = 0;
+	cout << "정적변수 a는 " << a << endl;
+}
+
+int main(void) {
+	jiyuk();
+	jiyuk();
+}
