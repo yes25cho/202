a64b9c37061b067f51d34f0caf8af94911724314
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-06 09:08:54 +0900

    매크로 함수 완성

--- 202/C++기본/02-01 매크로 함수.cpp
+++ 202/C++기본/02-01 매크로 함수.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 //pre컴파일러가 처리
 //자료형에 영향받지 않음
-#define jegob(A) A*A
+#define jegob(A) ((A)*(A))
 
 int main(void) 
 {	//연산자 우선순위 때문에 의도치 않은 결과가 나옴
 	cout << jegob(3+1) << endl;	//16
-	cout << 3+1 * 3+1 << endl;	//7
+	cout << (3+1) * (3+1) << endl;	//7
 	return 0;
 }
