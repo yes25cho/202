985fb67442a86d3dd0d90946681645039b047fca
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-06 09:20:21 +0900

    inline 함수

---
+++ 202/C++기본/02-02 inline 함수.cpp
@@ -0,0 +1,22 @@
+//cout, endl 객체를 가지고 있음
+#include <iostream>
+//std::  안써도 된다,.
+using namespace std;
+
+//pre컴파일러가 처리
+//자료형에 영향받지 않음
+//#define jegob(A) ((A)*(A))
+
+//컴파일러가 처리
+//자료형에 영향 받음
+//디버딘이 수월함
+inline int jegob(int A) {
+	return A * A;
+}
+
+int main(void) 
+{	//연산자 우선순위 때문에 의도치 않은 결과가 나옴
+	cout << jegob(3+1) << endl;	//16
+	cout << (3+1) * (3+1) << endl;	//16
+	return 0;
+}
