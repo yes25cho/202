8791b9392fe903b9c205bc90cca96398ca8aa2e1
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-07 10:59:33 +0900

    디폴트 매개변수(함수 오버로딩과는 별개)

--- 202/C++기본/02-05 함수 오버라이딩.cpp
+++ 202/C++기본/02-05 함수 오버라이딩.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 //함수 이름은 같고, 매개변수는 다름(함수 오버로딩)
-int jegob(int A, int B) {
+int jegob(int A, int B=1) {
 	return A * A + B;
 }
-double jegob(double A, double B) 
+
+double jegob(double A, double B=1.1) 
 {
 	return A * A + B;
 }
 
 int main(void) 
 {	
-	cout <<jegob(4, 1) << endl;		//17
-	cout << jegob(3.3, 1.1) << endl;		//11.99
+	cout <<jegob(4) << endl;		//jegob(4, 1)17
+	cout << jegob(3.3) << endl;		//jegob(3.3, 1.1)11.99
 	
 	return 0;
 }
