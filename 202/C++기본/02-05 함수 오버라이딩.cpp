de1899f68571c1425456f2c9e73fca7292b1b787
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-07 10:45:14 +0900

    함수 오버로딩

---
+++ 202/C++기본/02-05 함수 오버라이딩.cpp
@@ -0,0 +1,21 @@
+//cout, endl 객체를 가지고 있음
+#include <iostream>
+//std::  안써도 된다,.
+using namespace std;
+
+//함수 이름은 같고, 매개변수는 다름(함수 오버로딩)
+int jegob(int A, int B) {
+	return A * A + B;
+}
+double jegob(double A, double B) 
+{
+	return A * A + B;
+}
+
+int main(void) 
+{	
+	cout <<jegob(4, 1) << endl;		//17
+	cout << jegob(3.3, 1.1) << endl;		//11.99
+	
+	return 0;
+}
