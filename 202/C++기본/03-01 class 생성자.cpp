e09a551fd882d08b56b58b90379017e79278e42f
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-20 09:48:26 +0900

    멤버 변수를 class바깥에 정의

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -13,15 +13,8 @@ public:
 	//생성자 : 객체가 생성 될때 , 호출되는 함수
 	//디폴트 생성자를 컴파일러가 만들어 준다
 	//Student(){}
-	Student(void) {
-		nHakbun = 1234;
-		sName = "이사랑";
-		cout << "학번이 등록 되었습니다." << endl;
-	}
-	void show(void) {
-		cout << "학번은 " << nHakbun << "입니다." << endl;
-		cout << "이름은 " << sName << "입니다." << endl<<endl;
-	}
+	Student(void);
+	void show(void);
 };
 
 int main(void) {
@@ -29,4 +22,14 @@ int main(void) {
 	stu1.show();
 	Student stu2 = Student();
 	stu2.show();
+}
+
+Student::Student(void) {
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록 되었습니다." << endl;
+}
+void Student::show(void) {
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
 }
