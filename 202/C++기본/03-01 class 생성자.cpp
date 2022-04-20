fa9be035a4898fe6065fe7f2e7683a6347846eae
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-20 10:17:12 +0900

    생성자 오버로딩

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -9,21 +9,22 @@ class Student {
 public:
 	int nHakbun;
 	string sName;
-	
-	//생성자 : 객체가 생성 될때 , 호출되는 함수
-	//디폴트 생성자를 컴파일러가 만들어 준다
-	//Student(){}
 	Student(void);
+	Student(int Hakbun, string name);
 	void show(void);
 };
 
 int main(void) {
 	Student stu1;
 	stu1.show();
-	Student stu2 = Student();
+	Student stu2 = Student(1111, "JWP");
 	stu2.show();
 }
-
+Student::Student(int Hakbun, string name) {
+	nHakbun = Hakbun;
+	sName = name;
+	cout << "학번이 등록 되었습니다." << endl;
+}
 Student::Student(void) {
 	nHakbun = 1234;
 	sName = "이사랑";
