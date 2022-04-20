e9fd0afd8c2b3582b026efb032930c7422c4412f
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-20 09:17:10 +0900

    생성자 호출

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -5,13 +5,19 @@
 using namespace std;
 
 class Student {
-private:
+//private:
+public:
 	int nHakbun;
 	string sName;
 	
 	//생성자 : 객체가 생성 될때 , 호출되는 함수
 	//디폴트 생성자를 컴파일러가 만들어 준다
 	//Student(){}
+	Student(void) {
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록 되었습니다." << endl;
+	}
 };
 
 int main(void) {
