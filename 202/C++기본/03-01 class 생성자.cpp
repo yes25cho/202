f309089258f8a2ef0e38553f8cb2956c708ddcc2
yscho010612@gmail.com <yscho010612@gmail.com>
2022-05-12 12:08:24 +0900

    전통적인 문자열 방법으로 생성자 변경

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -1,6 +1,6 @@
 //cout, endl 객체를 가지고 있음
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -8,20 +8,16 @@ class Student {
 //private:
 public:
 	int nHakbun;
-	string sName;
-	Student(void);
-	Student(int Hakbun, string name);
+	//string sName;
+	char* sName;
+	Student(int Hakbun, const char* name);
 	void show(void);
 };
 
 int main(void) {
-	Student* stu = new Student[6];
-	int i;
-	for (i = 0; i < 6; i++) {
-		stu[i].show();
-	}
+	Student stu1 =  Student(1111, "JWP");
+	stu1.show();
 
-	delete [] stu;
 	return 0;
 }
 //Student::Student(int Hakbun, string name) {
@@ -29,20 +25,17 @@ int main(void) {
 //	this->sName = name;
 //	cout << "학번이 등록 되었습니다." << endl;
 //}
-Student::Student(int Hakbun, string name)
+Student::Student(int Hakbun, const char* name)
 //객체 생성과 동시에 맴버변수 초기화	//멤버 변수(매개변수)
 //const/참조형 멤버변수를 사용할 수 있다.
-	:nHakbun(Hakbun), sName(name)
+	:nHakbun(Hakbun)
 {
-	this->nHakbun = Hakbun;
-	this->sName = name;
-	cout << "학번이 등록 되었습니다." << endl;
+	cout << "일반 생성자 호출" << endl;
+	int len = strlen(name) + 1;		//동적할당항 공간의 크기를 구하고
+	sName = new char[len];			//그 크기만틈 배열로 동적 할당
+	strcpy(sName,name);				//매개변수에 있는 문자열 복사
 }
-Student::Student(void) {
-	this->nHakbun = 1234;
-	this->sName = "이사랑";
-	cout << "학번이 등록 되었습니다." << endl;
-}
+
 void Student::show(void) {
 	cout << "학번은 " << nHakbun << "입니다." << endl;
 	cout << "이름은 " << sName << "입니다." << endl << endl;
