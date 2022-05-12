d4419e93135f9fa3aa3f192f43fe23317a617dcd
yscho010612@gmail.com <yscho010612@gmail.com>
2022-05-12 12:15:48 +0900

    소멸자 추가

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -11,6 +11,7 @@ public:
 	//string sName;
 	char* sName;
 	Student(int Hakbun, const char* name);
+	~Student();
 	void show(void);
 };
 
@@ -25,6 +26,9 @@ int main(void) {
 //	this->sName = name;
 //	cout << "학번이 등록 되었습니다." << endl;
 //}
+
+
+
 Student::Student(int Hakbun, const char* name)
 //객체 생성과 동시에 맴버변수 초기화	//멤버 변수(매개변수)
 //const/참조형 멤버변수를 사용할 수 있다.
@@ -34,6 +38,11 @@ Student::Student(int Hakbun, const char* name)
 	int len = strlen(name) + 1;		//동적할당항 공간의 크기를 구하고
 	sName = new char[len];			//그 크기만틈 배열로 동적 할당
 	strcpy(sName,name);				//매개변수에 있는 문자열 복사
+}
+
+Student::~Student() {
+	delete[]sName;
+	cout << "소멸자 호출" << endl;
 }
 
 void Student::show(void) {
