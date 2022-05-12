36016f34f8254785bba419e16afb9952f877df70
yscho010612@gmail.com <yscho010612@gmail.com>
2022-05-12 11:03:13 +0900

    배열 동적할당

--- 202/C++기본/03-02 동적 할당.cpp
+++ 202/C++기본/03-02 동적 할당.cpp
@@ -4,8 +4,12 @@
 using namespace std;
 
 int main(void){
-	int* ptr1 = new int;		//int 형 공간 동적할당
-	*ptr1 = 20;
-	cout << *ptr1<<endl;
-	delete ptr1;
+	int* ptr2 = new int[4];
+	for (int i = 0; i < 4; i++) {
+		ptr2[i] = 10 + i;
+		cout<< ptr2[i] << " ";
+	}
+	delete [] ptr2;
+
+	return 0;
 }
