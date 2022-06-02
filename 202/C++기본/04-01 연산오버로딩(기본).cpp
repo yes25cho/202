845f149f9bc4deda57a9f08061412fcf0386abc0
yscho010612@gmail.com <yscho010612@gmail.com>
2022-06-02 11:39:33 +0900

    연산자 오버로딩 기본

--- 202/C++기본/04-01 연산오버로딩(기본).cpp
+++ 202/C++기본/04-01 연산오버로딩(기본).cpp
@@ -8,8 +8,17 @@ using namespace std;
 class  Point {
 public:
 	Point(void);
+	//생성자
 	Point(int X, int Y);
 	void print();
+	//연산자 오버로딩
+	Point operator+(const Point& rhs)
+	{
+		Point p;
+		p.mX = mX + rhs.mX;
+		p.mY = mY + rhs.mY;
+		return p;
+	}
 private:
 	int mX;
 	int mY;
@@ -17,8 +26,15 @@ private:
 
 int main(void) {
 	Point p1(1, 2);
+	Point p2(3, 4);
 	p1.print();
+
+	Point sum = p1 + p2;
+	sum.print();		//(4, 6)
 	return 0;
+}
+
+Point::Point(void) {
 }
 
 Point::Point(int X, int Y)
